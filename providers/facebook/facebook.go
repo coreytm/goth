46c276b754bc4aeb12d3474efd3b0b0b4ba2d1b2
Corey McGrillis <corey.mcgrillis@flybits.com>
2017-01-16 13:08:30 -0500

    added SignRequests boolean to Facebook provider. added code for generating proof. and making signed req

--- providers/facebook/facebook.go
+++ providers/facebook/facebook.go
@@ -4,8 +4,11 @@ package facebook
 
 import (
 	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -18,17 +21,18 @@ import (
 const (
 	authURL         string = "https://www.facebook.com/dialog/oauth"
 	tokenURL        string = "https://graph.facebook.com/oauth/access_token"
-	endpointProfile string = "https://graph.facebook.com/me?fields=email,first_name,last_name,link,about,id,name,picture,location"
+	endpointProfile string = "https://graph.facebook.com/me?fields=email,first_name,last_name,link,bio,id,name,picture,location"
 )
 
 // New creates a new Facebook provider, and sets up important connection details.
 // You should always call `facebook.New` to get a new Provider. Never try to create
 // one manually.
-func New(clientKey, secret, callbackURL string, scopes ...string) *Provider {
+func New(clientKey, secret, callbackURL string, signRequests bool, scopes ...string) *Provider {
 	p := &Provider{
-		ClientKey:   clientKey,
-		Secret:      secret,
-		CallbackURL: callbackURL,
+		ClientKey:    clientKey,
+		Secret:       secret,
+		CallbackURL:  callbackURL,
+		SignRequests: signRequests,
 	}
 	p.config = newConfig(p, scopes)
 	return p
@@ -40,6 +44,9 @@ type Provider struct {
 	Secret      string
 	CallbackURL string
 	config      *oauth2.Config
+
+	// SignedRequests is used to specify whether or not the provider is expecting signed requests
+	SignRequests bool
 }
 
 // Name is the name used to retrieve this provider later.
@@ -59,6 +66,21 @@ func (p *Provider) BeginAuth(state string) (goth.Session, error) {
 	return session, nil
 }
 
+// GetProof will create a sha256 hash of the user access token using the provider secret
+func (p *Provider) GetProof(session goth.Session) ([]byte, error) {
+	sess := session.(*Session)
+	h := hmac.New(sha256.New, []byte(p.Secret))
+
+	_, err := h.Write([]byte(sess.AccessToken))
+	if err != nil {
+		return nil, err
+	}
+
+	proof := h.Sum(nil)
+
+	return proof, nil
+}
+
 // FetchUser will go to Facebook and access basic information about the user.
 func (p *Provider) FetchUser(session goth.Session) (goth.User, error) {
 	sess := session.(*Session)
@@ -68,13 +90,39 @@ func (p *Provider) FetchUser(session goth.Session) (goth.User, error) {
 		ExpiresAt:   sess.ExpiresAt,
 	}
 
-	response, err := http.Get(endpointProfile + "&access_token=" + url.QueryEscape(sess.AccessToken))
-	if err != nil {
-		return user, err
-	}
-	defer response.Body.Close()
+	var resp *http.Response
 
-	bits, err := ioutil.ReadAll(response.Body)
+	if p.SignRequests {
+		proof, err := p.GetProof(session)
+		if err != nil {
+			return user, err
+		}
+
+		url := fmt.Sprintf(endpointProfile+"&appsecret_proof=%x", proof)
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return user, err
+		}
+
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", sess.AccessToken))
+		c := http.DefaultClient
+		resp, err = c.Do(req)
+		if err != nil {
+			return user, err
+		}
+	} else {
+		resp, err := http.Get(endpointProfile + "&access_token=" + url.QueryEscape(sess.AccessToken))
+		if err != nil {
+			if resp != nil {
+				resp.Body.Close()
+			}
+			return user, err
+		}
+	}
+
+	defer resp.Body.Close()
+
+	bits, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
 		return user, err
 	}
@@ -90,14 +138,12 @@ func (p *Provider) FetchUser(session goth.Session) (goth.User, error) {
 
 func userFromReader(reader io.Reader, user *goth.User) error {
 	u := struct {
-		ID        string `json:"id"`
-		Email     string `json:"email"`
-		About     string `json:"about"`
-		Name      string `json:"name"`
-		FirstName string `json:"first_name"`
-		LastName  string `json:"last_name"`
-		Link      string `json:"link"`
-		Picture   struct {
+		ID      string `json:"id"`
+		Email   string `json:"email"`
+		Bio     string `json:"bio"`
+		Name    string `json:"name"`
+		Link    string `json:"link"`
+		Picture struct {
 			Data struct {
 				URL string `json:"url"`
 			} `json:"data"`
@@ -113,11 +159,9 @@ func userFromReader(reader io.Reader, user *goth.User) error {
 	}
 
 	user.Name = u.Name
-	user.FirstName = u.FirstName
-	user.LastName = u.LastName
 	user.NickName = u.Name
 	user.Email = u.Email
-	user.Description = u.About
+	user.Description = u.Bio
 	user.AvatarURL = u.Picture.Data.URL
 	user.UserID = u.ID
 	user.Location = u.Location.Name
